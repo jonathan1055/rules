0490f2ad2f42e39fe7462794391833be23c35386
Jonathan Smith <jonathan1055@sandfordsolutions.com>
2020-09-22 11:46:53 +0100

    back to using Symfony for all

--- src/Event/GenericEvent.php
+++ src/Event/GenericEvent.php
@@ -6,13 +6,16 @@ namespace Drupal\rules\Event;
 // assist with deprecations and the transition to Symfony 5.
 // @todo Remove this when core 9.1 is the lowest supported version.
 // @see https://www.drupal.org/project/scheduler/issues/3166688
-if (!class_exists('Drupal\Component\EventDispatcher\Event')) {
-  class_alias('Symfony\Component\EventDispatcher\GenericEvent', 'Drupal\Component\EventDispatcher\Event');
-}
-use Drupal\Component\EventDispatcher\Event;
+// if (!class_exists('Drupal\Component\EventDispatcher\Event')) {
+//   class_alias('Symfony\Component\EventDispatcher\GenericEvent', 'Drupal\Component\EventDispatcher\Event');
+// }
+// use Drupal\Component\EventDispatcher\Event;
+
+use Symfony\Component\EventDispatcher\GenericEvent as AAA;
+
 
 /**
  * Class from which all Rules events are extended from.
  */
-class GenericEvent extends Event {
+class GenericEvent extends AAA {
 }
