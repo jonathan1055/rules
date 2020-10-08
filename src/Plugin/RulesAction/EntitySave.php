62d365f9eca711f391e085f25fb8836fbc92ff4f
Jonathan Smith <jonathan1055@sandfordsolutions.com>
2020-10-08 11:26:59 +0100

    add yes/no to entity save immediate

--- src/Plugin/RulesAction/EntitySave.php
+++ src/Plugin/RulesAction/EntitySave.php
@@ -22,6 +22,7 @@ use Drupal\rules\Core\RulesActionBase;
  *       label = @Translation("Force saving immediately"),
  *       description = @Translation("Usually saving is postponed till the end of the evaluation, so that multiple saves can be fold into one. If this set, saving is forced to happen immediately."),
  *       assignment_restriction = "input",
+ *       options_provider = "\Drupal\rules\Plugin\OptionsProvider\YesNoOptions",
  *       default_value = FALSE,
  *       required = FALSE
  *     ),
